60fdac7aa581e57258b5fa5aaa3692af22835620
Niloofar Zarifi <49527718+nzarifi@users.noreply.github.com>
2022-02-04 11:20:12 -0500

    Add files via upload

---
+++ CarClassLibrary2/CarClassLibrary/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public Car()
+        {
+            Make = "Nothing";
+            Model = "Nothing";
+            Price = 0.00M;
+
+        }
+        public Car(string a, string b, decimal c)
+        {
+            Make = a;
+            Model = b;
+            Price = c;
+        }
+
+        // To print c from carlist properly
+        override public string ToString()
+        {
+            return "Make:" + Make + " Model " + Model + " Price: $" + Price;
+        }
+    }
+}
---
+++ CarClassLibrary2/CarClassLibrary/Store.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarClassLibrary
+{
+    public class Store
+    {
+        public List<Car> Carlist { get; set; }
+        public List<Car> ShoppingList { get; set; }
+
+        public Store()
+        {
+            Carlist = new List<Car>();
+            ShoppingList = new List<Car>();
+        }
+
+        public decimal Checkout()
+        {
+            decimal totalcost = 0;
+            foreach (var c in ShoppingList)
+            {
+                totalcost += c.Price;
+            }
+            ShoppingList.Clear();
+            return totalcost;   
+
+        }
+    }
+}
